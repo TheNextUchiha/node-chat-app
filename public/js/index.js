f95c93adb84a4738307e43a78a101675519ca732
Farzan <farzanpira3@gmail.com>
2020-06-15 12:16:13 +0530

    add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+var socket = io();
+
+socket.on('connect', function () {
+    console.log('Connected to Server');
+
+    socket.on('newMessage', function(message) {
+        console.log('Got a new message', message);
+    });
+
+    socket.emit('createMessage', {
+        from: 'farzan@example.com',
+        text: 'Konnichiwa'
+    });
+});
+
+socket.on('disconnect', function () {
+    console.log('Disconnected form the server');
+});
--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'uchiha@example.com',
+        text: 'Hello',
+        createdAt: 123456
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('Message recieved', message);
+    });
+
     socket.on('disconnect', () => {
         console.log('User disconnected/left');
     });
